998bff61b6750d2485390a97482a0966f2ec2a5b
Kee <kieranm0104@gmail.com>
2025-11-11 17:32:40 -0500

    scoremaker
    scoremaker and saver

---
+++ Class10maybe/Assets/scripts/gamemanager.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using System.IO;
+using TMPro;
+using UnityEngine.InputSystem;
+
+public class gamemanager : MonoBehaviour
+{
+
+    //variables
+    //add a number
+    public int score;
+
+    //add a text
+    public TextMeshProUGUI text;
+
+    //add a text for scores
+    public TextMeshProUGUI scores;
+
+    //add a input reference
+    public InputActionReference interact;
+
+    //add a end reference
+    public InputActionReference end;
+
+    //sets the folder
+    private const string DIR_RES = "/Resources";
+
+    //sets the file
+    private const string FILE_SAVES = DIR_RES + "/Saves.txt";
+
+    //creates path to file
+    private string FILE_PATH_SAVES;
+
+    //string to add to .txt
+    public string inputnum;
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        score = 0;
+        //declare file path
+        FILE_PATH_SAVES = Application.dataPath + FILE_SAVES;
+
+        scores.text = File.ReadAllText(FILE_PATH_SAVES);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "" + score;
+
+        if (interact.action.WasPressedThisFrame())
+        {
+            score++;
+        }
+
+        if (end.action.WasPressedThisFrame())
+        {
+            SaveScore(score);
+
+            score = 0;
+        }
+    }
+
+    public void SaveScore(int s)
+    {
+        inputnum = s.ToString();
+
+        string fileContents = File.ReadAllText(FILE_PATH_SAVES);
+
+        fileContents += inputnum + "\n";
+
+        File.WriteAllText(FILE_PATH_SAVES, fileContents);
+    }
+
+
+}
